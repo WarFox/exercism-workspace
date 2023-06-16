c3922ca903325b7979ef5852330a34ab0af9d2f2
Deepu Mohan Puthrote <git@deepumohan.com>
2023-06-16 20:27:04 +0100

    refactor rotational-cipher :recycle:

--- clojure/rotational-cipher/src/rotational_cipher.clj
+++ clojure/rotational-cipher/src/rotational_cipher.clj
@@ -1,25 +1,22 @@
 (ns rotational-cipher)
 
-(defn rotate-with-wrap [letter rot z]
-  (let [ascii (int letter)
-        new-ascii (+ ascii (mod rot 26))]
-    (char
-     (if (> new-ascii z)
-       (- new-ascii 26)
-       new-ascii))))
+(defn rotate-with-wrap
+  "Rotate the given character by rot, with 'a' as the lowest character.
+   The result is within [a a+26]"
+  [ch rot a]
+  (let [ascii (int ch)
+        new-ascii (+ a (mod (+ rot (- ascii a))
+                            26))]
+    (char new-ascii)))
 
-(defn rotate-letter [letter rot]
-  (if (Character/isLowerCase letter)
-    (rotate-with-wrap letter rot (int \z))
-    (rotate-with-wrap letter rot (int \Z))))
+(defn rotate-char
+  [ch rot]
+  (if (Character/isLetter ch)
+    (if (Character/isLowerCase ch)
+      (rotate-with-wrap ch rot (int \a))
+      (rotate-with-wrap ch rot (int \A)))
+    ch))
 
-(defn rotate [text rot]
-  (loop [remaining text
-         acc       []]
-    (if (empty? remaining)
-      (apply str acc)
-      (let [ch     (first remaining)
-            new-ch (if (Character/isLetter ch)
-                     (rotate-letter ch rot)
-                     ch)]
-        (recur (rest remaining) (conj acc new-ch))))))
+(defn rotate
+  [text rot]
+  (apply str (map #(rotate-char %1 rot) text)))
